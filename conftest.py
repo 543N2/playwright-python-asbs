df53ad086011e7f1c91d0b98b5b3783d87bcadb8
543n2 <543n2.one@gmailcom>
2025-12-16 10:21:46 -0500

    Created project structure

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
+# Setting browser and page as fixtures, to execute on every test
+
+
+@pytest.fixture(scope="session")
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        yield browser
+        browser.close()
+
+
+@pytest.fixture
+def page(browser):
+    page = browser.new_page()
+    yield page
+    page.close()
